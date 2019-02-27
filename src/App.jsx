07003dcfeb9ecf0227933642e15c0e7cadd7c44b
Евгений Минин <evgeny.frontend@gmail.com>
2019-02-27 15:36:36 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './common/style.scss';
+
+class App extends Component {
+    render() { 
+        return (
+            <h3 className='project__title'>Hello React</h3>
+        );
+    }
+}
+ 
+export default App;
